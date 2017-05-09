98ceb644d9f4596306454937b3447333455b2a47
ankitpatnaik <ankit.patnaik91@gmail.com>
2017-05-09 14:45:32 -0500

    Update selection-sort.cpp

--- Assignment_2/selection-sort.cpp
+++ Assignment_2/selection-sort.cpp
@@ -6,24 +6,38 @@
 // Description : Implementation of selection sort in C++
 //============================================================================
 
-#include "sort.h"
+//#include "sort.h"
 #include <iostream>
+using namespace std;
 
-void SelectionSort::sort(int A[], int size)				// main entry point
+void selection_sort(int A[], int size)				// main entry point
 {
   /* Complete this function with the implementation of selection sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
-
   Ascending order
   */
-for (i=0;i<size;i++)
+  int i,j,min_;
+
+for (i=0;i<size-1;i++)
+{
+	min_= i;
 	for (j=i+1;j<size;j++)
 	{
-		if (A[i]>A[j])
-			{
-				temp=A[i];
-				A[i]=A[j+1];
-				A[j+1]=temp;
-			}	
+		if (A[j]<A[min_])
+		min_=j;
+	
 	}
+		int temp=A[i];
+		A[i]=A[min_];
+		A[min_]=temp;
+}
+}
+
+int main(){
+int arrayy[]={4,1,7,3,2};
+selection_sort(arrayy,5);
+
+for (int i=0;i<5;i++)
+	{cout<<arrayy[i]<<endl;}
+
 }
